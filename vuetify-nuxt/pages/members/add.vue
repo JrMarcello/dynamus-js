3ec68cc97bea710b5e39eb4281f55239f3aea926
Marcello Jr <marcello.dudk@gmail.com>
2018-06-08 07:22:33 -0300

    vuuuuu

--- vuetify-nuxt/pages/members/add.vue
+++ vuetify-nuxt/pages/members/add.vue
@@ -1,15 +1,39 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      
-    </v-flex>
-  </v-layout>
+  <v-container fluid grid-list-md>
+    <v-layout>
+      <v-flex>
+        <v-toolbar>
+          <v-toolbar-title>Cadastro de Membros</v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+          <v-flex d-flex>
+            <v-form>
+              <v-text-field v-model="member.name" label="Nome" id="id-name" />
+            </v-form>
+          </v-flex>
+          <v-flex d-flex>
+            <v-form md6>
+              <v-text-field v-model="member.name" label="Nome" id="id-name" />
+            </v-form>
+          </v-flex>
+          <!-- <v-card-title primary-title>
+            Dados Pessoais
+          </v-card-title>
+          <v-form md6>
+            <v-text-field v-model="member.name" label="Nome" id="id-name" />
+          </v-form> -->
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     data: () => ({
-
+      member: {
+        name: null
+      }
     })
   }
 </script>
